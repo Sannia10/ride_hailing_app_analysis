59e973f3185b0fb1d90e1589326a5e43a6a78a8a
Sannia Nasir <72022258+Sannia10@users.noreply.github.com>
2023-08-20 20:47:36 +0500

    Add files via upload

--- ride data.R
+++ ride data.R
@@ -1,7 +1,7 @@
 getwd()
 setwd("C:/Users/Sannia/Desktop/Sproj")
-pivot <- read.csv(file = "All Rides Data(1 Mrach 2017 - 31 June 2017) Sporj Lums -22-02-2018.csv")
-extra <- read.csv(file = "All Rides Data(1 Mrach 2017 - 31 June 2017) Sporj Lums -22-02-2018.csv")
+pivot <- read.csv(file = "Book1.csv")
+extra <- read.csv(file = "EXTRA.csv")
 view(pivot)
 
 # ride time with total fare
@@ -24,12 +24,6 @@ ggplot(extra, aes(x=extra$Count.of.status, y=extra$Average.of.driver_rating))+
 
 
 
-head(ride_data)
-ggplot(ride_data, aes(x=ride_data$status))+
-  geom_bar()
-pivot_data<- read.csv("pivot.csv")
-View(pivot_data)
-ggplot(pivot_data, aes(x=pivot_data$Count.of.status,y=pivot_data$Average.of.driver_rating))+
-  geom_point().
+
 
 
